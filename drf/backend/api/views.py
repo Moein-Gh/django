54408bb3e61477331e3859fece5823dafc4c436a
Moein-Gh <moeinghorbanali.biz@gmail.com>
2024-07-17 23:10:06 +0200

    up to 1:07

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,4 +1,14 @@
 from django.http import JsonResponse
+from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(["GET"])
 def api_home(request,*args,**kwargs):
-    return JsonResponse({"message":"Hi there this is your django API response"})
+    
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data , fields=['id','price'])
+    return JsonResponse(data)
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -3,6 +3,6 @@ import requests
 # endpoint = "http://localhost:8000/"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint) 
+get_response = requests.get(endpoint,json={"product_id":123})
 # print(get_response.text)
 print(get_response.json())
