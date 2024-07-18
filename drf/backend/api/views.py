57d23c230ed6609f8477fa5ccf6d122b3005d67f
moegh840 <moein.ghorbanali@uu.se>
2024-07-18 15:40:47 +0200

    Refactor API views and serializers

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,15 +1,14 @@
-from django.http import JsonResponse
 from products.models import Product
-from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
 
-@api_view(["GET"])
-def api_home(request,*args,**kwargs):
-    
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data , fields=['id','price'])
-    return JsonResponse(data)
+@api_view(["POST"])
+def api_home(request, *args, **kwargs):
+
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        # instance = serializer.save()
+        print(serializer.data)
+    return Response(serializer.data)
